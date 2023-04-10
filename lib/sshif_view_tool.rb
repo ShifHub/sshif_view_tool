49ff85a74f42f9225e415d786cd0f73d14607064
Sarrick <57643991+ShifHub@users.noreply.github.com>
2023-04-09 18:51:10 -0700

    View tool gem intial commit

---
+++ lib/sshif_view_tool.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "sshif_view_tool/version"
+require_relative "sshif_view_tool/renderer"
+
+
+module SshifViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sshif_view_tool/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SshifViewTool
+  VERSION = "0.1.0"
+end
